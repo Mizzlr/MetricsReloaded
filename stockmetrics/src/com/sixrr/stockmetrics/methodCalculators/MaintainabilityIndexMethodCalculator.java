65d76fa1d2f6c5bf115cc11863fc9aa88eeef083
Mushtaque Ahamed A <mushtaque@codenation.co.in>
2017-03-30 13:41:21 +0530

    Minor fix.

--- stockmetrics/src/com/sixrr/stockmetrics/methodCalculators/MaintainabilityIndexMethodCalculator.java
+++ stockmetrics/src/com/sixrr/stockmetrics/methodCalculators/MaintainabilityIndexMethodCalculator.java
@@ -25,7 +25,6 @@
 
 package com.sixrr.stockmetrics.methodCalculators;
 
-import com.intellij.psi.Math;
 import com.intellij.psi.JavaRecursiveElementVisitor;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiMethod;
